73111ce50049fa624aec39db5af677ae43a76e4f
Sós Gellért <gellertsos@gmail.com>
2025-12-03 21:17:01 +0100

    setup auth service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,26 @@
 import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true
+  }),
+);
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API'});
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+const port = process.env.PORT || 6001;
+
+const server = app.listen(port, () => {
+  console.log(`Auth service is running at http://localhost:${port}/api`);
+});
+
+server.on("error", (err) => {
+  console.log("Server error: ", err);
 });
