9370a5de82819bb5153154b7497b437bd09ac89a
Sós Gellért <gellertsos@gmail.com>
2025-12-03 13:54:32 +0100

    set middlewares in api-gateway service

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -15,11 +15,31 @@ app.use(
     allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true
   }),
-)
+);
+app.use(morgan("dev"));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cookieParser());
 
-app.get('/api', (req, res) => {
+app.set("trust proxy", 1);
+
+// Apply rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: (req: any) => (req.user ? 1000 : 100),
+  message: { error: "Too many requests, please try again later!" },
+  standardHeaders: true,
+  legacyHeaders: true,
+  keyGenerator: (req: any) => req.ip,
+});
+
+app.use(limiter);
+
+app.get('/gateway-health', (req, res) => {
   res.send({ message: 'Welcome to api-gateway!' });
 });
+
+app.use("/", proxy("http://localhost:6001"));
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
