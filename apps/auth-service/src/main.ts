4e9e1b61339a263b90702238f4394725b6911b54
Sós Gellért <gellertsos@gmail.com>
2025-12-05 11:00:45 +0100

    set error middleware in auth service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import { errorMiddleware } from "../../../packages/error-handler/error-middleware";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -11,9 +13,14 @@ app.use(
   }),
 );
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send({ message: "Hello API" });
 });
+
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 6001;
 
