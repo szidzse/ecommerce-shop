77d99075ab0aab2cfd5cdeb43895d388b1b745c5
Sós Gellért <gellertsos@gmail.com>
2025-12-05 17:22:21 +0100

    use auth router

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { errorMiddleware } from "../../../packages/error-handler/error-middleware";
 import cookieParser from "cookie-parser";
+import router from "./routes/auth.router";
 
 const app = express();
 
@@ -19,6 +20,9 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send({ message: "Hello API" });
 });
+
+// Routes
+app.use("/api", router);
 
 app.use(errorMiddleware);
 
